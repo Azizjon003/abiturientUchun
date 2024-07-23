dc7f6c5ab37c92c4bd0f2acc9606028f7c8e7bc9
Azizjon <aliqulovazizjon68@gmail.com>
2024-07-23 17:51:02 +0500

    feat: Update help message with instructions for using the bot

--- src/scenes/control.ts
+++ src/scenes/control.ts
@@ -17,4 +17,43 @@ scene.hears("⚖️ Kirish imkoniyatingizni baholash", async (ctx: any) => {
   return await ctx.scene.enter("universities");
 });
 
+scene.hears("Yordam 🆘", async (ctx) => {
+  console.log("Yordam");
+  ctx.reply(
+    `# Yordam 🆘
+
+Assalomu alaykum! Botimizdan foydalanish bo'yicha qo'llanma:
+
+1️⃣ Ro'yxatdan o'tish:
+   - Botni ishga tushiring va "/start" buyrug'ini yuboring.
+
+2️⃣ Universitetni tanlash:
+   - Ro'yxatdan o'tgandan so'ng, universitet nomini yozing yoki taklif qilingan ro'yxatdan tanlang.
+
+3️⃣ AbituriyentID kiritish:
+   - Tanlov yakunlangandan so'ng sizga berilgan AbituriyentID raqamini kiriting.
+
+4️⃣ Natijalarni ko'rish:
+   - Bot sizga kontrakt va grant asosida kirishingiz mumkin bo'lgan yo'nalishlarni ko'rsatadi.
+   - Har bir tur uchun alohida xabar yuboriladi.
+
+5️⃣ Sahifalarni ko'rish:
+   - Natijalar bir nechta sahifaga bo'lingan bo'lishi mumkin.
+   - "Oldinga ➡️" va "⬅️ Orqaga" tugmalaridan foydalanib, barcha natijalarni ko'rishingiz mumkin.
+
+❗️ Muammolar yuzaga kelganda:
+   - Agar xato xabarini olsangiz, ko'rsatmalarga rioya qilganingizga ishonch hosil qiling.
+   - Agar muammo davom etsa, "/start" buyrug'ini yuborib, jarayonni qaytadan boshlang.
+
+Agar qo'shimcha savollaringiz bo'lsa, iltimos, botdan foydalanishda davom eting. Biz sizga yordam berishdan mamnun bo'lamiz!
+
+Murojaat uchun: @aliqulov_azizjon :)
+Omad tilaymiz! 🍀
+`,
+    {
+      parse_mode: "HTML",
+    }
+  );
+});
+
 export default scene;
